f385e9f7094f6edf7abba759a26d5c2eb5e7fc91
marginasit305 <82200246+marginasit305@users.noreply.github.com>
2023-09-27 12:05:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var main = document.querySelector(".header");
+var nav = document.querySelector(".nav");
+
+var offset = main.offsetHeight - nav.offsetHeight;
+
+window.onscroll = function () {
+  if (window.pageYOffset > offset) {
+    nav.classList.remove("bottom-nav");
+    nav.classList.add("top-nav");
+  } else {
+    nav.classList.add("bottom-nav");
+    nav.classList.remove("top-nav");
+  }
+};
